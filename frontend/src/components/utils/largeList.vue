ba1287397a859d8123e13dd2b3ce9184df72cb6b
luciocape <luciodimascapezzuto@gmail.com>
2025-07-02 01:58:01 -0300

    refactor(styles.scss, typeInput.vue, Buy.vue, largeList.vue(new)): buy view frontend finished

---
+++ frontend/src/components/utils/largeList.vue
@@ -0,0 +1,90 @@
+<template>
+	<div
+		v-if="items.length >= 1"
+		class="position-relative items-list-container"
+	>
+		<span
+			class="d-flex justify-content-center align-items-center number-of-items position-absolute rounded-circle p-2"
+			>{{ items.length }}</span
+		>
+		<div class="items w-100 bg-body rounded-3 ms-3 overflow-hidden">
+			<ul
+				class="border border-2 border-info list-group d-flex flex-column m-0 bg-body rounded-3"
+			>
+				<li
+					class="list-group-item m-0 pt-1 w-100 d-flex"
+					v-for="item in items"
+				>
+					<span class="text w-75 overflow-x-hidden">{{ item }}</span
+					><IconTrash
+						v-if="trashAllowed"
+						@click="$emit( 'deleteItem', items.indexOf(item))"
+						size="25px"
+						color="rgb(0, 124, 146)"
+						class="icon-trash"
+					/>
+				</li>
+			</ul>
+		</div>
+	</div>
+</template>
+
+<script setup>
+//import { ref } from 'vue';
+import IconTrash from "../icons/iconTrash.vue";
+
+name: "LargeList";
+
+const props = defineProps({
+	items: {
+		type: Array,
+		default: [],
+	},
+	trashAllowed: {
+		type: Boolean,
+		default: false,
+	},
+});
+//const emits = defineEmits([]);
+</script>
+
+<style scoped lang="scss">
+@import "../../assets/styles.scss";
+.items-list-container {
+	width: 45%;
+	max-width: 200px;
+	.number-of-items {
+		width: 30px;
+		height: 30px;
+		right: -20px;
+		box-shadow: 3px -3px 10px -3px rgba(var(--bs-secondary-rgb), 0.75);
+		top: -10px;
+		background: linear-gradient(var(--bs-bg) 0%, $primary 100%) !important;
+		aspect-ratio: 1/1 !important;
+		z-index: 199;
+	}
+	.items {
+		height: 40px;
+		&:hover {
+			overflow: visible !important;
+		}
+		ul {
+			height: fit-content;
+			width: calc(100% + 17px);
+			max-height: 250px;
+			overflow-y: scroll;
+			li {
+				.text {
+					display: inline-block;
+				}
+				.icon-trash {
+					&:hover {
+						cursor: pointer;
+						scale: 1.1;
+					}
+				}
+			}
+		}
+	}
+}
+</style>
--- frontend/src/views/Buy.vue
+++ frontend/src/views/Buy.vue
@@ -1,11 +1,18 @@
 <template>
 	<main class="container mt-5">
+		<div class="d-flex gap-2 align-items-center mb-4 bg-secondary bg-opacity-75 rounded-5 p-1 text-white" style="max-width: 200px;">
+			<div class="rounded-circle bg-primary" style="width: 60px; height: 60px;"></div>
+			<div class="d-flex flex-column align-items-start">
+				<span class="h5 font-times mb-1">Your balance</span>
+				<span class="h3 font-times m-0">$15000</span>
+			</div>
+		</div>
 		<section class="d-grid">
 			<div class="row gap-4">
 				<div
 					class="col recipe-img d-flex flex-column justify-content-center align-items-center gap-2"
 				>
-					<div class="bg-primary w-100 food"></div>
+					<div class="bg-primary w-100 first"></div>
 					<div class="d-flex gap-2 w-100">
 						<div
 							v-for="img in 3"
@@ -14,20 +21,33 @@
 					</div>
 				</div>
 				<article
-					class="col text-secondary buying-data rounded-4 border border-2 border-secondary mx-3 p-4"
+					class="col d-flex flex-column gap-3 text-secondary buying-data rounded-4 border border-2 border-secondary mx-3 p-4 "
 				>
-					<div class="d-flex justify-content-end fw-semibold font-times">
+					<div
+						class="d-flex justify-content-end fw-semibold font-times"
+					>
 						<span>100 vendidos</span>
 					</div>
-					<h2 class="font-times h4">Fernando Alonso</h2>
-					<h1 class="">Pato a la mostasa</h1>
-					<Stars size="27px" :shadowed="true" stars="4" />
-					<div class="font-cookie display-4">$2000</div>
-					<div
-						class="fw-medium font-cookie h3 d-flex align-items-center gap-2"
-					>
-						<IconClock color="rgb(0, 124, 146)" />30min
+					<div class="d-flex flex-column gap-2">
+						<header>
+							<h2 class="font-times h4">Fernando Alonso</h2>
+							<h1 class="h2">Pato a la mostasa</h1>
+							<Stars size="27px" :shadowed="true" stars="4" />
+						</header>
+						<div>
+							<div class="font-times display-5">$2000</div>
+							<div
+								class="fw-medium font-times h5 d-flex align-items-center gap-2 m-0"
+							>
+								<IconClock color="rgb(0, 124, 146)" />30min
+							</div>
+							<h3 class="h3 font-times">Ingredients</h3>
+							<LargeList :items="items" />
+						</div>
 					</div>
+					<button class="w-100 py-2 btn btn-secondary buy-button font-times">
+						Buy
+					</button>
 				</article>
 			</div>
 		</section>
@@ -39,10 +59,21 @@
 import Stars from "../components/utils/stars.vue";
 import RecipeComents from "../components/recipe/recipeComents.vue";
 import IconClock from "../components/icons/iconClock.vue";
+import LargeList from "../components/utils/largeList.vue";
 
 //import { ref } from 'vue';
 
 name: "Buy";
+
+const items = [
+	"1 Pato",
+	"1 Mostaza",
+	"1 Cucharada de miel",
+	"1 Cucharada de aceite de oliva",
+	"1 Cucharada de salsa de soja",
+	"1 Cucharada de mostaza",
+	"1 Cucharada de miel",
+];
 
 //const props = defineProps({});
 //const emits = defineEmits([]);
@@ -53,9 +84,16 @@ name: "Buy";
 section {
 	.recipe-img {
 		min-width: 300px;
+		height: 70vh;
+		.first {
+			height: 100%;
+			max-height: 300px;
+			border-radius: 10px;
+		}
 		.food {
 			aspect-ratio: 3/2;
 			max-height: 250px;
+			border-radius: 10px;
 		}
 	}
 	.buying-data {
@@ -64,6 +102,9 @@ section {
 		background-color: #22ed7049;
 		.price {
 			font-family: "Cookie", cursive;
+		}
+		.buy-button{
+			font-size: 1.5rem;
 		}
 	}
 }
